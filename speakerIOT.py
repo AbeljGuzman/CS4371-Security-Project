7b33f21389c4124cda7a0e158d43a8ee5ee8766f
Dylan <jfz13@txstate.edu>
2024-04-17 17:46:12 -0500

    added comments

--- speakerIOT.py
+++ speakerIOT.py
@@ -1,6 +1,7 @@
 from IOTdevice import IOTDevice
 from data.config import *
 
+# Speaker IOT device simulator
 class SpeakerIOT(IOTDevice):
     state = None
     status = None
@@ -9,12 +10,15 @@ class SpeakerIOT(IOTDevice):
     def __init__(self, id):
         super().__init__(id)
         self.state = "off"
-        self.status = "Not Connected"
+        self.status = "not connected"
         self.volume = 0
         
+    # get state of the speaker
     def get_state(self):
         return self.state
     
+    # set state of the speaker
+    # set_state;on or set_state;off
     def set_state(self, state):
         if state == "on" or state == "off":
             self.state = state
@@ -22,21 +26,29 @@ class SpeakerIOT(IOTDevice):
         else:
             raise Exception("Invalid State: ", state)
     
+    # get status of the speaker (connected or not connected)
+    # get_status
     def get_status(self):
         return self.status
     
+    # set status of the speaker (connected or not connected)
+    # set_status;Connected or set_status;Cot Connected
     def set_status(self, state):
         if self.state == "off":
             raise Exception("Device is off")
-        if state == "Connected" or state == "Not Connected":
+        if state == "connected" or state == "not connected":
             self.status = state
             return "200"
         else:
             raise Exception("Invalid Status: ", state)
     
+    # get volume of the speaker
+    # get_volume
     def get_volume(self):
         return str(self.volume)
     
+    # set volume of the speaker
+    # set_volume;0-10
     def set_volume(self, volume):
         if self.state == "off":
             raise Exception("Device is off")
@@ -46,7 +58,8 @@ class SpeakerIOT(IOTDevice):
             return "200"
         else:
             raise Exception("Invalid Volume choice: ", volume)
-        
+    
+    # maps commands to their respective functions
     def process_command(self, command, message=None):
         mapper = {
             'get_state': self.get_state,
@@ -63,12 +76,12 @@ class SpeakerIOT(IOTDevice):
     
     
 if __name__ == "__main__":
-    speaker = SpeakerIOT("1010")
-    speaker.setEncryption(KEY, upperCaseAll=False, removeSpace=False)
+    speaker = SpeakerIOT("5") # initialize speaker IOT device
+    speaker.setEncryption(KEY, upperCaseAll=False, removeSpace=False) # set encryption
 
     print("Setting up a new Smart Speaker..")
-    input_ip = SPEAKER_IP
-    input_port = SPEAKER_PORT
+    input_ip = SPEAKER_IP # ensure IP is configured in config
+    input_port = SPEAKER_PORT # ensure port is configured in config
     speaker.init_sockets(input_ip, input_port)
     
     print("Receiving......")
