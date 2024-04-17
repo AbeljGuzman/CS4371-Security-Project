eddce592c92226cec1230a234618709cf76cc5dc
Dylan <jfz13@txstate.edu>
2024-04-17 17:45:44 -0500

    added comments

--- smartLightIOT.py
+++ smartLightIOT.py
@@ -1,13 +1,16 @@
 from IOTdevice import IOTDevice
 from data.config import *
 
+# SmartLight IOT device simulator
 class SmartLight(IOTDevice):
     def __init__(self, id):
         super().__init__(id)
-        self._state = "off"  
-        self._brightness = 0  
+        self._state = "off"
+        self._brightness = 50
         self._color = "white"
 
+    # sets the state of the smart light
+    # set_state;on or set_state;off
     def set_state(self, state):
         if state in ["on", "off"]:
             self._state = state
@@ -15,6 +18,8 @@ class SmartLight(IOTDevice):
         else:
             raise Exception("invalid message", state)
 
+    # sets the brightness of the smart light (smart light must be on to set brightness)
+    # set_brightness;0-100
     def set_brightness(self, brightness):
         print(f"Received brightness input: {brightness} of type {type(brightness)}")  # Debug: Check the input type and value
         
@@ -31,26 +36,32 @@ class SmartLight(IOTDevice):
             return "200"
         else:
             raise Exception("Invalid message", brightness)
-        
+    
+    # sets the color of the smart light (smart light must be on to set color)
+    # ex: set_color;red
     def set_color(self, color):
         if self._state == "off": 
             raise Exception("off")
         self._color = color
         return "200"  # success code
 
+    # returns the state of the smart light
     def get_state(self):
         return self._state
 
+    # returns the brightness of the smart light
     def get_brightness(self):
         if self._state == "off": 
             raise Exception("off")
         return str(self._brightness)
 
+    # returns the color of the smart light
     def get_color(self):
         if self._state == "off": 
             raise Exception("off")
         return self._color
 
+    # maps commands to their respective functions
     def process_command(self, command, message=None):
         try:
             mapper = {
@@ -73,18 +84,18 @@ class SmartLight(IOTDevice):
                 return f"ERROR: {e}"
 
 def main():
-    light = SmartLight("2")  
-    light.setEncryption(KEY, upperCaseAll=False, removeSpace=False)
+    light = SmartLight("4")  # initialize smart light IOT device
+    light.setEncryption(KEY, upperCaseAll=False, removeSpace=False) # set encryption
 
     print("Setting up a new Smart Light..")
-    input_ip = SMARTLIGHT_IP  # Ensure you have this configured in your config file
-    input_port = SMARTLIGHT_PORT  # Ensure this is also configured
+    input_ip = SMARTLIGHT_IP  # ensure IP is configured in config
+    input_port = SMARTLIGHT_PORT  # ensure port is configured in config
 
     light.init_sockets(input_ip, input_port)
 
     print("Receiving command from Hub")
 
-    # Receive, parse, and process command from Hub
+    # recieve, parse, and process command from Hub
     command = light.receive()
     while command != "exit":
         command, message = light.parse_command(command)
